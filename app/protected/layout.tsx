95d0f4d2608807a586f416a08a3341cf7f1ab409
Anees Malik <theaneesmalik@gmail.com>
2024-07-31 00:50:16 +0500

    added layout.tsx in protected

---
+++ app/protected/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+const defaultUrl = `${process.env.NEXT_PUBLIC_URL!}protected`
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'Next.js and Supabase Starter Kit',
+  description: 'The fastest way to build apps with Next.js and Supabase',
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const supabase = createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) {
+    return redirect('/login')
+  }
+  return <main className='min-h-screen flex flex-col items-center justify-center'>{children}</main>
+}
